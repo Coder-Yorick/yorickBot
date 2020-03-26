d10274473e55c50e132ee841d09d6da93c7599b4
Yorick <yorick.chen@kuke3d.com>
2020-03-26 17:35:41 +0800

    adjust mask reply

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ const InterpretLocation = function (lat, lng, source) {
             }
             let msgs = [];
             nearby_pharmacies.forEach(nearby_pharmacy => {
-                let title = `${MaskPharmacy.MaskIcon}${nearby_pharmacy.pharmacy.info.name}\n`;
+                let title = `${nearby_pharmacy.pharmacy.info.name}\n`;
                 title += `成人: ${nearby_pharmacy.mask.adult}個\n`
                 title += `兒童: ${nearby_pharmacy.mask.child}個\n`;
                 if (nearby_pharmacy.mask.note != null && nearby_pharmacy.mask.note.length > 0) {
