6113bee7ef507d207792459f3ad9ba80515e53be
Yorick <yorick.chen@kuke3d.com>
2020-08-19 10:44:08 +0800

    change stock info resource

--- Stock.js
+++ Stock.js
@@ -3,6 +3,7 @@ const cheerio = require('cheerio');
 const iconv = require('iconv-lite');
 
 const STOCK_WEBSITE = 'http://goodinfo.tw/StockInfo/StockDividendPolicy.asp?STOCK_ID=';
+const STOCK_TWSE = 'http://mis.twse.com.tw/stock/api/getStockInfo.jsp';
 const DIVIDEND_WEBSITE = 'https://www.moneydj.com/Z/ZG/ZGL/ZGL.djhtm';
 
 function Stock() {
@@ -28,6 +29,24 @@ function Stock() {
     }
 
     this.GetStockInfo = function (stockID) {
+        return new Promise((resolve, reject) => {
+            this.crawStockInfoFromTWSE(stockID).then(data => {
+                if (data === null) {
+                    this.crawStockInfoFromGoodinfo(stockID).then(gdata => {
+                        resolve(gdata);
+                    }).catch(e => {
+                        reject(e);
+                    });
+                } else {
+                    resolve(data);
+                }
+            }).catch(e => {
+                reject(e);
+            });
+        });
+    }
+
+    this.crawStockInfoFromGoodinfo = (stockID) => {
         return new Promise((resolve, reject) => {
             request(STOCK_WEBSITE + stockID, (err, res, body) => {
                 try {
@@ -56,6 +75,33 @@ function Stock() {
                     reject(e.message);
                 }
             });
+        });
+    }
+
+    this.crawStockInfoFromTWSE = (stockID) => {
+        return new Promise((resolve, reject) => {
+            try {
+                let current_ts = Math.floor(Date.now() / 1000);
+                let stockUrl = `${STOCK_TWSE}?_=${current_ts}&ex_ch=tse_${stockID}.tw`;
+                request({url: stockUrl, json: true}, (err, res, body) => {
+                    try {
+                        let dt = body.msgArray[0];
+                        let price = (dt['z'] != '-' ? dt['z'] : dt['o']) * 1;
+                        let yprice = dt['y'] * 1;
+                        resolve({
+                            name: `${stockID} ${dt['n']}`,
+                            price: price.toFixed(2) * 1,
+                            spread: (price - yprice).toFixed(2) * 1,
+                            dividend: '-'
+                        });
+                    } catch (ex) {
+                        console.log(ex.message);
+                        resolve(null);
+                    }
+                });
+            } catch (e) {
+                reject(e.message);
+            }
         });
     }
 
